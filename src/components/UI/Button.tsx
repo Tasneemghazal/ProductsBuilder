700801a09fc33bf0f20e65e4e4a86b2d87ac0a78
Tasneem <tasneemgazal5@gmail.com>
2024-02-15 09:15:12 +0200

    add rest attrubute to button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -7,6 +7,6 @@ className:string,
 
 export const Button = ({children,className,...rest}:Iprops) => {
   return (
-    <button className={`${className}  text-white w-full rounded-md p-1`}>{children}</button>
+    <button className={`${className}  text-white w-full rounded-md p-1`} {...rest}>{children}</button>
   )
 }
