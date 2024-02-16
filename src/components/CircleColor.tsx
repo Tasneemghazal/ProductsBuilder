270ff8d781ca9a076e5c6ccc570b1240e5e7381b
Tasneem <tasneemgazal5@gmail.com>
2024-02-16 11:55:51 +0200

    create circle color component

---
+++ src/components/CircleColor.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from "react"
+
+interface Iprops extends HTMLAttributes<HTMLSpanElement>{
+color: string,
+}
+
+const CircleColor = ({color,...rest}:Iprops) => {
+  return (
+    <span className="block w-5 h-5 bg-indigo-500 rounded-full cursor-pointer" style={{backgroundColor:color}} {...rest}/>
+  )
+}
+
+export default CircleColor
