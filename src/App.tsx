6ef0b3848f3a2474553a8efa2163dbc9435233b9
Tasneem <tasneemgazal5@gmail.com>
2024-02-16 11:55:29 +0200

    handle add product form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { ProductCard } from "./components/ProductCard.tsx";
 import MyDialog from "./components/UI/MyDialog.tsx";
-import { formInputsList, productList } from "./components/data/data.ts";
+import { colors, formInputsList, productList } from "./components/data/data.ts";
 import { Button } from "./components/UI/Button.tsx";
 import Input from "./components/UI/Input.tsx";
+import { IProduct } from "./components/interfaces/interfaces.ts";
+import { productSchema } from "./validation/index.ts";
+import Error from "./components/UI/Error.tsx";
+import CircleColor from "./components/CircleColor.tsx";
 
 const App = () => {
+  const defaultProductObject = {
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+    colors: [],
+    category: {
+      name: "",
+      imageURL: "",
+    },
+  };
+  const [product, setProduct] = useState<IProduct>(defaultProductObject);
   const [isOpen, setIsOpen] = useState(false);
+  const [tempColor, setTempColor] = useState<string[]>([]);
+  console.log(tempColor);
+  const [Errors, setErrors] = useState({
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+  });
 
   function closeModal() {
     setIsOpen(false);
@@ -15,25 +39,87 @@ const App = () => {
   function openModal() {
     setIsOpen(true);
   }
+  const onChangeHandler = (
+    event: ChangeEvent<HTMLInputElement>,
+    inputName: string
+  ) => {
+    const newValue = event.target.value;
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      [inputName]: newValue,
+    }));
+    setErrors({
+      ...Errors,
+      [inputName]: "",
+    });
+  };
+
+  function submitHandler(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const errors = productSchema({
+      title: product.title,
+      description: product.description,
+      imageURL: product.imageURL,
+      price: product.price,
+    });
+
+    const hasErrorMessage =
+      Object.values(errors).some((value) => value == "") &&
+      Object.values(errors).every((value) => value == "");
+    if (!hasErrorMessage) {
+      setErrors(errors);
+      return;
+    }
+    console.log("product information submitted successfully");
+  }
+  function onCancel(): void {
+    setProduct(defaultProductObject);
+    closeModal();
+  }
+
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   const renderFormInputList = formInputsList.map((input) => (
-    <div className="flex flex-col ">
+    <div className="flex flex-col " key={input.id}>
       <label
         htmlFor={input.id}
         className="mb-[2px] text-sm font-medium text-gray-900"
       >
         {input.label}
       </label>
-      <Input type={input.type} id={input.id} name={input.name} />
+      <Input
+        type="text"
+        id={input.id}
+        name={input.name}
+        value={product[input.name]}
+        onChange={(event) => onChangeHandler(event, input.name)}
+      />
+      <Error msg={Errors[input.name]} />
     </div>
   ));
+  const renderColorList = colors.map((color) => (
+    <CircleColor
+      key={color}
+      color={color}
+      onClick={() => {
+        if(tempColor.includes(color)) {
+          setTempColor(prev=>prev.filter(item=>item !== color))
+          return;}
+        setTempColor((prev) => [...prev, color])
+      }}
+    />
+  ));
+  // the above syntax inside serTempColor to get the prev value of the state
+
   return (
-    <div className="container">
-      <Button className="bg-indigo-600 " onClick={openModal}>
-        Add
-      </Button>
+    <div className="container flex flex-col justify-center items-center">
+      <div className="w-full flex justify-center mt-3 ">
+        <Button className="bg-indigo-600 w-2/4 m-auto" onClick={openModal}>
+          Build Product
+        </Button>
+      </div>
+
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-5 gap-2 md:gap-4 p-2">
         {renderProductList}
       </div>
@@ -42,13 +128,24 @@ const App = () => {
         closeModal={closeModal}
         title={"Add New Product"}
       >
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputList}
+          <div className="flex my-2 space-x-2">{tempColor.map(color=>(
+            <span key={color} style={{backgroundColor:color}} className="p-1 mr-1 rounded-md text-white">{color}</span>
+          )
+          )}</div>
+          <div className="flex my-2 space-x-2">{renderColorList}</div>
+          
           <div className="flex items-center space-x-2">
             <Button className="bg-indigo-600 hover:bg-indigo-700">
               Submit
             </Button>
-            <Button className="bg-gray-300 hover:bg-gray-500">Close</Button>
+            <Button
+              className="bg-gray-300 hover:bg-gray-500"
+              onClick={onCancel}
+            >
+              Close
+            </Button>
           </div>
         </form>
       </MyDialog>
